ca7c171f8493a6b6a5c5293477a60d6bcb54398b
Seungwoo Hong <hsw5781@gmail.com>
2017-11-22 16:08:14 +0900

    categorizing completed, localization uncompleted

--- webserver01/data01/views.py
+++ webserver01/data01/views.py
@@ -30,15 +30,15 @@ def index(request):
     today=datetime.today()
     tomorrow=today+timedelta(days=1)
     this_week=today.isocalendar()[1] #week number
-
+    date_KOR=["월", "화", "수", "목", "금", "토", "일"]
     #today_list, tomorrow_list, this_week_list에는 각각 오늘 내일 이번주 강연 데이터가 들어있음
     #각각 조건에 맞는 filter를 이용하였음.
     today_list=pd1_time.objects.filter(date__year=today.year, date__month=today.month, date__day=today.day)
     tomorrow_list=pd1_time.objects.filter(date__year=tomorrow.year, date__month=tomorrow.month, date__day=tomorrow.day)
     this_week_list=pd1_time.objects.filter(date__week=this_week)
-    return render(request,'data01/index.html',today_list)
-    #contents={'today_list':today_list,'tomorrow_list':tomorrow_list,'this_week_list':this_week_list}
-    #return render(request,'data01/index.html',contents)
+    #return render(request,'data01/index.html',today_list)
+    contents={'date_KOR':date_KOR, 'today_list':today_list,'tomorrow_list':tomorrow_list,'this_week_list':this_week_list}
+    return render(request,'data01/index.html',contents)
 '''
 def about(request):
     return render(request,'about site')
